c4f27e80944d5af96890c35ddf10c06896aa6fbe
thenareshofficial <thenareshoffcial@gmail.com>
2023-10-20 09:34:06 -0700

    Background Color

---
+++ background_color.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+        printf("\n\033[1;40m Black color \n");
+        printf("\033[1;41m Red color \n");
+        printf("\033[1;42m Green color \n");
+        printf("\033[1;43m Yellow color \n");
+        printf("\033[1;44m Blue color \n");
+        printf("\033[1;45m Purple color \n");
+        printf("\033[1;46m Cyan color \n");
+        printf("\033[1;47m White color \n");
+}
