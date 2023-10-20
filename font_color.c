cef3586904eda64b9c242eb9965aea05d338e85e
thenareshofficial <thenareshoffcial@gmail.com>
2023-10-20 09:34:48 -0700

    Font Color

---
+++ font_color.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+        printf("\n\033[1;30m [+] Black color \n");
+        printf("\033[1;31m [+] Red color \n");
+        printf("\033[1;32m [+] Green color \n");
+        printf("\033[1;33m [+] Yellow color \n");
+        printf("\033[1;34m [+] Blue color \n");
+        printf("\033[1;35m [+] Purple color \n");
+        printf("\033[1;36m [+] Cyan color \n");
+        printf("\033[1;37m [+] White color \n");
+}
